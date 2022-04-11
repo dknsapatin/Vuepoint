a78be67811feb09a3efc5682aa688d8766f7851e
Denzel Sapatin <dknsapatin@gmail.com>
2022-04-11 14:45:43 -0400

    Added Map section. | Omitted CloudGoogleMap API Key

--- src/App.js
+++ src/App.js
@@ -13,14 +13,18 @@ const App = () => {
     <>
       <CssBaseline />
       <Header />
+
       <Grid container spaceing={3} style={{ width: "100%" }}>
+        {/* ----------------------------------------------------------- */}
         {/* Only take 4 out of 12 spaces on medium or larger devices */}
         <Grid item xs={12} md={4}>
           <List />
         </Grid>
+        {/* ----------------------------------------------------------- */}
         <Grid item xs={12} md={8}>
           <Map />
         </Grid>
+        {/* ----------------------------------------------------------- */}
       </Grid>
     </>
   );
